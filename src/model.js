ea6adfa7672ad7c55651dbdae34bdf85c89d1d65
Nicolás Scarcella <nscarcella@gmail.com>
2017-07-09 02:13:10 -0300

    minor changes

--- src/model.js
+++ src/model.js
@@ -14,19 +14,9 @@ export const Test = (description) => (...sentences) => Node('Program')({ descrip
 //-------------------------------------------------------------------------------------------------------------------------------
 
 export const Package = (name) => (...elements) => Node('Package')({ name, elements })
-export const ClassDeclaration = (name) => (superclass = SuperType()(), ...mixins) => (...members) => Node('ClassDeclaration')({
-  name,
-  superclass,
-  mixins,
-  members
-})
+export const ClassDeclaration = (name) => (superclass = SuperType()(), ...mixins) => (...members) => Node('ClassDeclaration')({ name, superclass, mixins, members })
 export const MixinDeclaration = (name) => (...members) => Node('MixinDeclaration')({ name, members })
-export const ObjectDeclaration = (name = undefined) => (superclass = SuperType()(), ...mixins) => (...members) => Node('NamedObjectDeclaration')({
-  name,
-  superclass,
-  mixins,
-  members
-})
+export const ObjectDeclaration = (name = undefined) => (superclass = SuperType()(), ...mixins) => (...members) => Node('NamedObjectDeclaration')({ name, superclass, mixins, members })
 
 //-------------------------------------------------------------------------------------------------------------------------------
 // MEMBERS
