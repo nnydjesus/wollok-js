57388343455c3f301eb47db45bfcbcc68c94d2d1
Javier Fernandes <javier.fernandes@gmail.com>
2017-07-10 09:11:31 -0300

    Refactoring some part of the interpreter code. In progress.. to show to nico to continue

--- src/interpreter.js
+++ src/interpreter.js
@@ -40,26 +40,27 @@ import {
 // I know, I know... But this is not meant to be nice or final code. Just a quick rough approach to get a better feeling on the AST shape.
 
 const compileSentence = (sentence) => {
-  if(sentence.nodeType === 'VariableDeclaration') {
-    const { variable, writeable, value } = sentence
-    return ` ${writeable ? 'let' : 'const'} ${variable.name} = ${compileExpression(value)} ;`
+  switch (sentence.nodeType) {
+    case 'VariableDeclaration': {
+      const { variable, writeable, value } = sentence
+      return ` ${writeable ? 'let' : 'const'} ${variable.name} = ${compileExpression(value)} ;`
+    }
+    case 'Assignment': {
+      const { variable, value } = sentence
+      return ` ${variable.name} = ${compileExpression(value)} ;`
+    }
+    default: return `${compileExpression(sentence)};`
   }
-  if(sentence.nodeType === 'Assignment') {
-    const { variable, value } = sentence
-    return ` ${variable.name} = ${compileExpression(value)} ;`
-  }
-
-  return compileExpression(sentence) + ';'
 }
 
 
 const compileExpression = (expression) => {
-  if(expression.nodeType === 'Variable') {
+  if (expression.nodeType === 'Variable') {
     const { name } = expression
     return ` ${name} `
   }
   
-  if(expression.nodeType === 'BinaryOp') {
+  if (expression.nodeType === 'BinaryOp') {
     const { op, left, right } = expression
     const leftOperand = compileExpression(left)
     const rightOperand = compileExpression(right)
@@ -87,7 +88,7 @@ const compileExpression = (expression) => {
 
   }
   
-  if(expression.nodeType === 'UnaryOp') {
+  if (expression.nodeType === 'UnaryOp') {
     const { op, target } = expression
     const operand = compileExpression(target)
 
@@ -101,37 +102,37 @@ const compileExpression = (expression) => {
     }
   }
   
-  if(expression.nodeType === 'InstanceOf') {
+  if (expression.nodeType === 'InstanceOf') {
     const { left, right } = expression
     return `${compileExpression(left)} instanceof ${right}`
   }
   
-  if(expression.nodeType === 'FeatureCall') { //TODO: nullSafe
+  if (expression.nodeType === 'FeatureCall') { //TODO: nullSafe
     const { target, key, nullSafe, parameters } = expression
     return `${compileExpression(target)}["${key}"](${compileArguments(parameters)})`
   }
   
-  if(expression.nodeType === 'New') {
+  if (expression.nodeType === 'New') {
     const { target, parameters } = expression
     return `new ${target}(${compileArguments(parameters)})`
   }
-  
-  if(expression.nodeType === 'Super') {
+
+  if (expression.nodeType === 'Super') {
     const { parameters } = expression
     return `super(${compileArguments(parameters)})`
   }
-  
-  if(expression.nodeType === 'If') {
+
+  if (expression.nodeType === 'If') {
     const { condition, thenSentences, elseSentences } = expression
-    return `(()=>{if(${compileExpression(condition)}){${ compileSentenceSequence(thenSentences) }}else{${ compileSentenceSequence(elseSentences) }}})()`
+    return `(()=>{if (${compileExpression(condition)}){${ compileSentenceSequence(thenSentences) }}else{${ compileSentenceSequence(elseSentences) }}})()`
   }
-  
-  if(expression.nodeType === 'Try') {
+
+  if (expression.nodeType === 'Try') {
     const { sentences, catches, always } = expression
 
     const handlers = catches.map(({variable, type, handler}) => {
       const evaluation = `const ${variable.name} = ___ERROR___;${compileSentenceSequence(handler)}`
-      return type ? `if(___ERROR___ instanceof ${type}){${evaluation}}` : evaluation
+      return type ? `if (___ERROR___ instanceof ${type}){${evaluation}}` : evaluation
     })
 
     const catchBlock = catches.length ? `catch(___ERROR___){${handlers.join(';')} throw ___ERROR___}` : ''
@@ -140,12 +141,12 @@ const compileExpression = (expression) => {
     return `(()=>{try{${compileSentenceSequence(sentences)}}${catchBlock}${alwaysBlock}})()`
   }
 
-  if(expression.nodeType === 'Throw') {
+  if (expression.nodeType === 'Throw') {
     const { exception } = expression
     return `(()=>{throw ${compileExpression(exception)}})()`
   }
 
-  if(expression.nodeType === 'Return') {
+  if (expression.nodeType === 'Return') {
     const { result } = expression
     return `return ${compileExpression(result)}`
   }
@@ -155,57 +156,37 @@ const compileExpression = (expression) => {
 
 
 const compileLiteral = (literal) => {
-  if(literal.nodeType === 'NullLiteral') {
-    return `null`
+  const rules = {
+    NullLiteral: () => 'null',
+    SelfLiteral: () => 'this',
+    BooleanLiteral: ({ value }) => `${value}`,
+    NumberLiteral: ({ value }) => `${value}`,
+    StringLiteral: ({ value }) => `"${value}"`,
+    SetLiteral: ({ values }) => `new Set([ ${values.map(compileExpression).join()} ])`,
+    ListLiteral: ({ values }) => `[ ${values.map(compileExpression).join()} ]`
+  }
+  if (rules[literal.nodeType]) {
+    return rules[literal.nodeType](literal)
   }
 
-  if(literal.nodeType === 'SelfLiteral') {
-    return `this`
-  }
-
-  if(literal.nodeType === 'BooleanLiteral') {
-    const { value } = literal
-    return `${value}`
-  }
- 
-  if(literal.nodeType === 'NumberLiteral') {
-    const { value } = literal
-    return `${value}`
-  }
- 
-  if(literal.nodeType === 'StringLiteral') {
-    const { value } = literal
-    return `"${value}"`
-  }
- 
-  if(literal.nodeType === 'SetLiteral') {
-    const { values } = literal
-    return `new Set([ ${values.map(compileExpression).join() } ])`
-  }
- 
-  if(literal.nodeType === 'ListLiteral') {
-    const { values } = literal
-    return `[ ${values.map(compileExpression).join() } ]`
-  }
- 
-  if(literal.nodeType === 'Closure') {
+  if (literal.nodeType === 'Closure') {
     const { parameters, sentences } = literal
     const compiledSentences = sentences.map(compileSentence)
-    if(compiledSentences.length) compiledSentences[compiledSentences.length - 1] = 'return ' + compiledSentences[compiledSentences.length - 1]
+    if (compiledSentences.length) compiledSentences[compiledSentences.length - 1] = `return ${compiledSentences[compiledSentences.length - 1]}`
 
-    return `(function (${ compileParameters(parameters) }) {${ compileSentenceSequence(sentences) }})`
+    return `(function (${compileParameters(parameters)}) {${compileSentenceSequence(sentences)}})`
   }
 }
 
 // TODO: Handle mixin inclusion
 const compileElement = (element) => {
 
-  if(element.nodeType === 'Package') {
+  if (element.nodeType === 'Package') {
     const { name, elements } = element
-    //TODO
+    // TODO
   }
 
-  if(element.nodeType === 'ClassDeclaration') {
+  if (element.nodeType === 'ClassDeclaration') {
     const { name, superclass, mixins, members } = element
     const constructorDeclaration = compileConstructor(
       members.filter(m => m.nodeType === 'VariableDeclaration' && m.value),
@@ -219,12 +200,12 @@ const compileElement = (element) => {
     return `class ${name} extends ${superclass.name} { ${constructorDeclaration} ${memberDeclarations} }`
   }
 
-  if(element.nodeType === 'MixinDeclaration') {
+  if (element.nodeType === 'MixinDeclaration') {
     const { name, members } = element
-    //TODO
+    // TODO
   }
 
-  if(element.nodeType === 'ObjectDeclaration') {
+  if (element.nodeType === 'ObjectDeclaration') {
     const { name, superclass, mixins, members } = element
     return `const ${name} = new class extends ${superclass.name}{
       constructor(){super(${superclass.compileArguments(parameters)})};${members.map(compileMember).join(';')}
@@ -234,27 +215,25 @@ const compileElement = (element) => {
 }
 
 const compileMember = (member) => {
-  if(member.nodeType === 'VariableDeclaration'){
+  if (member.nodeType === 'VariableDeclaration') {
     const { variable, writeable, value } = member
     const getter = `get ['${variable.name}']() {return this['___${variable.name}___']}`
     const setter = `set ['${variable.name}'](___value___) {this['___${variable.name}___'] = ___value___}`
   }
   
-  if(member.nodeType === 'MethodDeclaration') { //TODO: override? Native?
+  if (member.nodeType === 'MethodDeclaration') { //TODO: override? Native?
     const { name, override, native, parameters, sentences } = member
     return `['${name}'](${compileParameters(parameters)}){${compileSentenceSequence(sentences)}}`
   }
 }
 
 const compileConstructor = (constructors, variableDeclarations) => {
-
   const constructorFunctions = constructors.map(({ parameters, sentences, baseTarget, baseArguments }) =>
     `___cons___[${baseArguments.length}] = (${compileParameters(parameters)}) => {
       ${baseTarget === SelfLiteral ? `___cons___['${baseArguments.length}']` : 'super'}(${compileArguments(baseArguments)});
       ${compileSentenceSequence(sentences)}
     }`
   )
-  
   return `constructor(...___args___){___cons___ = {};${constructorFunctions.join(';')}; ___cons___[___args___.length](...___args___)}`
 }  
 
@@ -264,36 +243,36 @@ const compileArguments = (args) => args.map(compileExpression).join()
 
 const compileSentenceSequence = (sentences) => {
   const compiledSentences = sentences.map(compileSentence)
-  if(compiledSentences.length && !compiledSentences[compiledSentences.length - 1].startsWith('return'))
-    compiledSentences[compiledSentences.length - 1] = 'return ' + compiledSentences[compiledSentences.length - 1]
+  if (compiledSentences.length && !compiledSentences[compiledSentences.length - 1].startsWith('return'))
+    compiledSentences[compiledSentences.length - 1] = `return ${compiledSentences[compiledSentences.length - 1]}`
   return compiledSentences.join('')
 }
 
-//TODO: Imports
-const compileFile = ({content}) => {
-  const imports = content.filter(c => c.nodeType === 'Import')
-  const programs = content.filter(c => c.nodeType === 'Program')
-  const tests = content.filter(c => c.nodeType === 'Test')
-  const elements = content.filter(c => c.nodeType !== 'Test' && c.nodeType !== 'Program' && c.nodeType != 'Import')
+const findByType = (content, type) => content.filter(c => c.nodeType === type)
 
-  if(!programs.length && !tests.length) {
-    log.error(`There is no program or tests to run!`)
+//TODO: Imports
+const compileFile = ({ content }) => {
+  const imports = findByType(content, 'Import')
+  const programs = findByType(content, 'Program')
+  const tests = findByType('Test')
+  const elements = content.filter(c => c.nodeType !== 'Test' && c.nodeType !== 'Program' && c.nodeType !== 'Import')
+
+  if (!programs.length && !tests.length) {
+    log.error('There is no program or tests to run!')
     process.exit(1)
   }
-  
+
   const compiledElements = elements.map(compileElement).join(';')
 
-  if(programs.length) {
-    if(programs.length > 1) log.warn(`There is more than one program! Only ${programs[0].name} will be run.`)
+  if (programs.length) {
+    if (programs.length > 1) log.warn(`There is more than one program! Only ${programs[0].name} will be run.`)
     const { name, sentences } = programs[0]
-    return compiledElements + `;(()=>{${compileSentenceSequence(sentences)}})()`
-  } else {
-    //TODO: Run tests
-    tests.map(({ description, sentences }) => {
-    })
+    return `${compiledElements} ;(()=>{${compileSentenceSequence(sentences)}})()`
   }
+  // TODO: Run tests
+  tests.map(({ description, sentences }) => {
+  })
 }
-
 
 export const interpretSentence = (ast) => eval(compileSentence(ast))
 export const interpretElement = (ast) => eval(compileElement(ast))
