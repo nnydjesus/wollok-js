c0a649cf3b7ac2907f89fa2b811bfbd02c5b8843
Nicolás Scarcella <nscarcella@gmail.com>
2017-07-12 13:35:39 -0300

    sorting things

--- src/model.js
+++ src/model.js
@@ -13,6 +13,8 @@ export const Class = (name) => (superclass = SuperType()(), ...mixins) => (...me
 export const Mixin = (name) => (...members) => Node('Mixin')({ name, members })
 export const Singleton = (name = undefined) => (superclass = SuperType()(), ...mixins) => (...members) => Node('NamedSingleton')({ name, superclass, mixins, members })
 
+export const SuperType = (name = 'Object') => (...parameters) => Node('SuperType')({ name, parameters })
+
 export const Program = (name) => (...sentences) => Node('Program')({ name, sentences: Block(...sentences) })
 export const Test = (description) => (...sentences) => Node('Program')({ description, sentences: Block(...sentences) })
 
@@ -37,7 +39,8 @@ export const Assignment = (variable, value) => Node('Assignment')({ variable, va
 //-------------------------------------------------------------------------------------------------------------------------------
 
 export const Variable = (name) => Node('Variable')({ name })
-
+export const Literal = (value) => Node('Literal')({ value })
+export const List = (...values) => Node('List')({ values })
 export const Closure = (...parameters) => (...sentences) => Node('Closure')({ parameters, sentences: Block(...sentences) })
 
 export const Send = (target, key, nullSafe = false) => (...parameters) => Node('Send')({ target, key, nullSafe, parameters })
@@ -47,23 +50,13 @@ export const New = (target) => (...parameters) => Node('New')({ target, paramete
 export const InstanceOf = (left, right) => Node('InstanceOf')({ left, right })
 
 export const If = (condition) => (...thenSentences) => (...elseSentences) => Node('If')({ condition, thenSentences: Block(...thenSentences), elseSentences: Block(...elseSentences) })
-export const Try = (...sentences) => (...catches) => (...always) => Node('Try')({ sentences: Block(...sentences), catches, always: Block(...always) })
 export const Throw = (exception) => Node('Throw')({ exception })
-
-//-------------------------------------------------------------------------------------------------------------------------------
-// LITERALS
-//-------------------------------------------------------------------------------------------------------------------------------
-
-export const Literal = (value) => Node('Literal')({ value })
-export const List = (...values) => Node('List')({ values })
-
-//-------------------------------------------------------------------------------------------------------------------------------
-// INNERS
-//-------------------------------------------------------------------------------------------------------------------------------
-
-export const SuperType = (name = 'Object') => (...parameters) => Node('SuperType')({ name, parameters })
-
+export const Try = (...sentences) => (...catches) => (...always) => Node('Try')({ sentences: Block(...sentences), catches, always: Block(...always) })
 export const Catch = (variable, type) => (...handler) => Node('Catch')({ variable, type, handler: Block(...handler) })
+
+//-------------------------------------------------------------------------------------------------------------------------------
+// COMMONS
+//-------------------------------------------------------------------------------------------------------------------------------
 
 export const Parameter = (name, varArg = false) => Node('Param')({ name, varArg })
 
