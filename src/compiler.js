dc8ba61f973e70bdd4aa018579a84688924a039b
Nicolás Scarcella <nscarcella@gmail.com>
2017-07-12 17:32:28 -0300

    cleaning up unused functions in compiler

--- src/compiler.js
+++ src/compiler.js
@@ -9,29 +9,25 @@ const { assign } = Object
 const compile = assign(expression => compile[expression.type](expression), {
   // TODO: PACKAGE: ({ name, elements }) => {},
 
-  Singleton: ({ name, superclass, members }) => // TODO: Mixin linearization
-    `const ${name} = new class extends ${superclass.name}{
-      constructor(){super(${compileArguments(superclass.parameters)})};${members.map(compile).join(';')}
+  Singleton: ({ name, superclass, superArguments, members }) => // TODO: Mixin linearization
+    `const ${name} = new class extends ${superclass}{
+      constructor(){super(${superArguments.map(compile).join()})};${members.map(compile).join(';')}
     };`,
 
   // TODO Mixin: ({ name, members }) => {},
 
-  Class: ({ name, superclass, members }) => { // TODO: Mixin linearization
-    const constructors = findByType(members, 'VariableDeclaration', m => m.value)
-    const variableDeclarations = findByType(members, 'Constructor')
-
-    const Constructor = `constructor(){
-      ${variableDeclarations.map(({ name, value }) => `this.${name}=${compile(value)}`)}.join(';')
-      ${constructors.map(compile)}
-      this.constructor['__init'+arguments.length+'__'].bind(this)(...arguments)
-    }`
-    const memberDeclarations = members.filter(m => m.type !== 'Constructor').map(compile).join(';')
-    return `class ${name} extends ${superclass.name} { ${Constructor} ${memberDeclarations} }`
-  },
+  Class: ({ name, superclass, members }) => // TODO: Mixin linearization
+    `class ${name} extends ${superclass} {
+      constructor() {
+        ${members.filter(m => m.type === 'Field').map(({ name, value }) => `this.${name}=${compile(value)}`)}.join(';')
+        this.constructor['__init'+arguments.length+'__'].bind(this)(...arguments)
+      }
+      ${members.map(compile).join(';')}
+    }`,
 
   Constructor: ({ parameters, sentences, lookUpCall, baseArguments }) =>
-    `static ___init${parameters.length}___(${compileParameters(parameters)}) {
-      ${lookUpCall ? 'super' : `this.constructor.__init'${baseArguments.length}___'`}(${compileArguments(baseArguments)});
+    `static ___init${parameters.length}___(${parameters.map(compile).join()}) {
+      ${lookUpCall ? 'super' : `this.constructor.__init'${baseArguments.length}___'`}(${baseArguments.map(compile).join()});
       ${compile(sentences)}
     }`,
 
@@ -43,7 +39,7 @@ const compile = assign(expression => compile[expression.type](expression), {
   },
 
   // TODO: override? Native?
-  Method: ({ name, parameters, sentences }) => `['${name}'](${compileParameters(parameters)}){${compile(sentences)}}`,
+  Method: ({ name, parameters, sentences }) => `['${name}'](${parameters.map(compile).join()}){${compile(sentences)}}`,
 
   VariableDeclaration: ({ variable, writeable, value }) => `${writeable ? 'let' : 'const'} ${variable.name} = ${compile(value)}`,
 
@@ -51,54 +47,14 @@ const compile = assign(expression => compile[expression.type](expression), {
 
   Variable: ({ name }) => `${name}`,
 
-  BinaryOp: ({ op, left, right }) => {
-    const rightOperand = compile(right)
-    const leftOperand = compile(left)
-
-    switch (op) { // TODO: Other Ops: '..<' / '>..' / '..' / '->' / '>>>' / '>>' / '<<<' / '<<' / '<=>' / '<>' / '?:'
-      case 'or': return `(${leftOperand} || ${rightOperand})`
-      case 'and': return `(${leftOperand} && ${rightOperand})`
-      case '===': return `Object.is(${leftOperand},${rightOperand})`
-      case '!==': return `!Object.is(${leftOperand},${rightOperand})`
-      case '==': return `(${leftOperand} === ${rightOperand})`
-      case '!=': return `(${leftOperand} !== ${rightOperand})`
-      case '**': return `Math.pow(${leftOperand},${rightOperand})`
-      case '||':
-      case '&&':
-      case '>=':
-      case '<=':
-      case '>':
-      case '<':
-      case '+':
-      case '-':
-      case '*':
-      case '/':
-      case '%': return `(${leftOperand} ${op} ${rightOperand})`
-      default: throw TypeError('Unsupported operator')
-    }
-  },
-
-  UnaryOp: ({ op, target }) => {
-    const operand = compile(target)
-    switch (op) {
-      case 'not':
-      case '!': return `!${operand}`
-      case '-': return `-${operand}`
-      case '++': return `${operand}++`
-      case '--': return `${operand}--`
-      default: throw TypeError('Unsupported operator')
-      // case '+': TODO: WTF does this do?
-    }
-  },
-
   InstanceOf: ({ left, right }) => `${compile(left)} instanceof ${right}`,
 
   // TODO: Nullsafe? Do we really want this?
-  Send: ({ target, key, parameters }) => `${compile(target)}["${key}"](${compileArguments(parameters)})`,
+  Send: ({ target, key, parameters }) => `${compile(target)}["${key}"](${parameters.map(compile).join()})`,
 
-  New: ({ target, parameters }) => `new ${target}(${compileArguments(parameters)})`,
+  New: ({ target, parameters }) => `new ${target}(${parameters.map(compile).join()})`,
 
-  Super: ({ parameters }) => `super(${compileArguments(parameters)})`,
+  Super: ({ parameters }) => `super(${parameters.map(compile).join()})`,
 
   If: ({ condition, thenSentences, elseSentences }) =>
     `(() => { if (${compile(condition)}) {${compile(thenSentences)}} else {${compile(elseSentences)}}})()`,
@@ -126,27 +82,13 @@ const compile = assign(expression => compile[expression.type](expression), {
 
   List: ({ values }) => `[ ${values.map(compile).join()} ]`,
 
-  Closure: ({ parameters, sentences }) => `(function (${compileParameters(parameters)}) {${compile(sentences)}})`,
+  Closure: ({ parameters, sentences }) => `(function (${parameters.map(compile).join()}) {${compile(sentences)}})`,
 
-  File: ({ content }) => {
-    // TODO: Imports const imports = findByType(content, 'Import')
-    const programs = findByType(content, 'Program')
-    const tests = findByType('Test')
-    const elements = content.filter(c => c.type !== 'Test' && c.type !== 'Program' && c.type !== 'Import')
+  File: ({ content }) => content.map(compile).join(';'),
+  // TODO: Imports
+  // TODO: tests
 
-    if (!programs.length && !tests.length) {
-      log.error('There is no program or tests to run!')
-      process.exit(1)
-    }
-
-    const compiledElements = elements.map(compile).join(';')
-
-    if (programs.length) {
-      if (programs.length > 1) log.warn(`There is more than one program! Only ${programs[0].name} will be run.`)
-      return `${compiledElements} ;(()=>{${compile(programs[0].sentences)}})()` // TODO: Extract Program compile
-    }
-    // TODO: Run tests tests.map(({ description, sentences }) => { })
-  },
+  Program: ({ name, sentences }) => `function ${name}(){${compile(sentences)}}`,
 
   Block: ({ sentences }) => {
     const compiledSentences = sentences.map(sentence => `${compile(sentence)};`)
@@ -154,14 +96,9 @@ const compile = assign(expression => compile[expression.type](expression), {
       compiledSentences[compiledSentences.length - 1] = `return ${compiledSentences[compiledSentences.length - 1]}`
     }
     return compiledSentences.join(';')
-  }
+  },
+
+  Parameter: ({ name, varArg }) => (varArg ? `...${name}` : name)
 })
-
-const compileParameters = (params) => params.map(({ name, varArg }) => (varArg ? `...${name}` : name)).join()
-
-const compileArguments = (args) => args.map(compile).join()
-
-// TODO: Wherever we use this, we should have separate field in the model instead
-const findByType = (content, type, extraCondition = () => true) => content.filter(c => c.type === type && extraCondition(c))
 
 export default compile
