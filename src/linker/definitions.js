9ea7a35988c25c1a1f57f75766408c3b4022b597
Javier Fernandes <javier.fernandes@gmail.com>
2017-07-19 23:17:03 -0300

    removing comment

--- src/linker/definitions.js
+++ src/linker/definitions.js
@@ -1,11 +1,10 @@
 import { Variable, New, Field, VariableDeclaration, Parameter, Block, Program, File, Class, Method, Closure, Singleton, Mixin } from '../model'
 
 // This declarations are not the best and most intuitive form, but
-// the minimum to add new cases already understandables by the curren linker impl
+// the minimum to add new cases already understandables by the current linker impl
 // I know that it is not yet close to the "final version" or "the best way
 //  to express links rules", but I'm iterating over it trying to understand the underlying model
 //  based on needs.
-
 
 /* nodes that define new scopes/namespaces */
 const scopeables = [
@@ -30,23 +29,6 @@ export const referenciables = {
   [Mixin.name]: byName,
   [Singleton.name]: byName
 }
-
-/** 
- * TODO: what should be linked are not whole nodes but some attribute of them
- * For example Class.superclass and Class.mixins
- * This should exppress that !
- */
-
-// links({
-//   Class: {
-//     superclass: Class,
-//     mixins: many(Mixin)
-//   },
-//   // 2 things here:
-//          1) the whole node gets replaced by the reference. 
-//          2) could be a ref to many thinks: Param | VariableDeclaration, for example
-//   Variable: Referenciable 
-// })
 
 export const linkeables = {
   [Variable.name]: 'name',
