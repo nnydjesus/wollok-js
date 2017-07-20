44e4bf2faf132870bbf2b4e876a7efd09adc7e61
Javier Fernandes <javier.fernandes@gmail.com>
2017-07-20 10:54:09 -0300

    adding tests for link type checking

--- tests/linker/definitions/link.checks.test.js
+++ tests/linker/definitions/link.checks.test.js
@@ -1,9 +1,31 @@
 import { expect } from 'chai'
 import { Parameter, Field } from '../../../src/model'
-import { or } from '../../../src/linker/types'
+import { a, or, many } from '../../../src/linker/types'
 import { Ref } from '../../../src/linker/steps/link'
 
 describe('Link checks', () => {
+
+  describe('a()', () => {
+    it('should say true for a ref of that type', () => {
+      expect(a(Parameter)(Ref(Parameter('sarasa')))).to.equals(true)
+    })
+    it('should say false for a ref of another type', () => {
+      expect(a(Parameter)(Ref(Field('sarasa')))).to.equals(false)
+    })
+  })
+
+  describe('many()', () => {
+    it('should say true if all elements comply', () => {
+      expect(many(Parameter)([Ref(Parameter('p1')), Ref(Parameter('p2'))])).to.equals(true)
+    })
+    it('should say false if none comply with the type', () => {
+      expect(many(Parameter)([Ref(Field('p1')), Ref(Field('p2'))])).to.equals(false)
+    })
+    // this case is still like.. under-designed
+    it('should say false if one of them does NOT comply', () => {
+      expect(many(Parameter)([Ref(Parameter('p1')), Ref(Field('p2'))])).to.equals(false)
+    })
+  })
 
   describe('or()', () => {
     it('should say true for a ref of that type', () => {
