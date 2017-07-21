8214e4f947feddb8d2b8de10eff7376ba5a17b78
Nicolás Scarcella <nscarcella@gmail.com>
2017-07-21 00:09:10 -0300

    adding index.js as main

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,11 +3,9 @@ import mocha from 'gulp-mocha'
 import eslint from 'gulp-eslint'
 import babel from 'gulp-babel'
 import pegjs from 'gulp-pegjs'
-import { exec } from 'child_process'
 import del from 'del'
 
 const task = gulp.task.bind(gulp)
-
 
 task('clean', () => del(['dist', '*.tgz']))
 
@@ -41,7 +39,3 @@ task('lint', () =>
     .pipe(eslint.format())
     .pipe(eslint.failAfterError())
 )
-
-task('pack', ['test'], () => exec('cp -t ./dist/ ./package.json ./README.md && cd ./dist && npm pack && cd ..'))
-
-task('default', ['lint', 'test'])
---
+++ src/index.js
@@ -0,0 +1,4 @@
+export { default as interpreter } from './interpreter'
+export { default as parser } from './parser'
+export { default as compiler } from './compiler'
+export { link as linker } from './linker/linker'
